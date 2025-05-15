a054d3342befc80c979dbf3120c02c4df9d2ebd7
Joshua Richardson <jrichardson@wearebond.com>
2025-05-14 22:10:57 -0700

    fix(ts): update ts and vite

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,17 @@
+import tailwindcss from '@tailwindcss/vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from '@tanstack/start/config'
 
 export default defineConfig({
   tsr: {
     appDirectory: 'src/app',
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+      tsConfigPaths({
+        projects: ['./tsconfig.json'],
+      }),
+    ],
   },
 })
