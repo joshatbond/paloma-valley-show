ba6061b9fcc732eee8f55b709f41c49bc065023b
Joshua Richardson <jrichardson@wearebond.com>
2025-06-21 15:54:29 -0700

    fix(admin): remove unneeded import

--- src/app/routes/admin.tsx
+++ src/app/routes/admin.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
 import { convexQuery, useConvexMutation } from '@convex-dev/react-query'
 import { api } from '~/server/convex/_generated/api'
