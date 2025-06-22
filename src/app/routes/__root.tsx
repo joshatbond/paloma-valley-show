311ce2acecfb06df31c14c663df2b69df00e7c29
Joshua Richardson <jrichardson@wearebond.com>
2025-06-22 15:45:22 -0700

    fix(site): title

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -17,7 +17,7 @@ export const Route = createRootRouteWithContext<{
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'Wildcat Brigade',
       },
     ],
     links: [{ rel: 'stylesheet', href: appCss }],
