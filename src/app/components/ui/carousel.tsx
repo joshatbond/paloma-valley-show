7a27c340d631d8dda972b9ed1a77cb728b7cf576
Joshua Richardson <jrichardson@wearebond.com>
2025-07-14 20:27:55 -0700

    feat(landing page): implement polish

--- src/app/components/ui/carousel.tsx
+++ src/app/components/ui/carousel.tsx
@@ -1,28 +1,24 @@
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
 
+const urls = ['conductor', 'drums', 'trumpet', 'tuba'].map(
+  (s) => `/images/${s}.png`
+)
+
 export function Carousel() {
   const [emblaRef] = useEmblaCarousel({}, [Autoplay()])
 
   return (
     <div className='overflow-hidden' ref={emblaRef}>
-      <div className='flex'>
-        <img
-          src='/images/conductor.png'
-          className='h-full object-contain shrink-0 grow-0 basis-full'
-        />
-        <img
-          src='/images/drums.png'
-          className='h-full object-contain shrink-0 grow-0 basis-full'
-        />
-        <img
-          src='/images/trumpet.png'
-          className='h-full object-contain shrink-0 grow-0 basis-full'
-        />
-        <img
-          src='/images/tuba.png'
-          className='h-full object-contain shrink-0 grow-0 basis-full'
-        />
+      <div className='flex h-[20vh]'>
+        {urls.map((url) => (
+          <div
+            key={url}
+            className='shrink-0 grow-0 basis-full flex justify-end'
+          >
+            <img className='object-contain' src={url} />
+          </div>
+        ))}
       </div>
     </div>
   )
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,11 +1,13 @@
 import { convexQuery } from '@convex-dev/react-query'
 import { useSuspenseQuery } from '@tanstack/react-query'
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute, Link, useNavigate } from '@tanstack/react-router'
 import { api } from '~/server/convex/_generated/api'
 import { ShowLoader } from '~/app/components/show/loader'
 import { Button } from '../components/ui/button'
 import { Controller } from '../components/ui/controller'
 import { Carousel } from '../components/ui/carousel'
+import { useEffect, useState } from 'react'
+import { useHaptic } from '../hooks/useHaptic'
 
 export const Route = createFileRoute('/')({
   component: Home,
@@ -13,32 +15,78 @@ export const Route = createFileRoute('/')({
 
 function Home() {
   const { data } = useSuspenseQuery(convexQuery(api.appState.get, {}))
+  const navigation = useNavigate()
+  const haptics = useHaptic()
+  const [showSelected, showSelectedAssign] = useState(false)
+
+  useEffect(() => {
+    showSelectedAssign(!!data.showId)
+  }, [data])
 
   return (
     <main className='min-h-screen grid grid-rows-[1fr_auto]'>
       <div className='bg-[#222] text-white px-8 py-4 grid'>
-        <div className='relative overflow-clip flex flex-col pt-12 h-full bg-black rounded'>
-          <div className='absolute inset-x-0 bottom-0 h-[20vh]'>
+        <div className='relative overflow-clip flex flex-col pt-8 h-full bg-black rounded'>
+          <div className='absolute inset-x-2 bottom-2 h-[20vh]'>
             <Carousel />
           </div>
 
           <div className='flex justify-center'>
-            <img src='/images/logo.png' className='w-[80%]' />
+            <img src='/images/logo.png' className='w-[75%]' />
           </div>
-          {data.showId && <ShowLoader />}
 
-          <Link to='/program'>
-            <Button>View the program</Button>
-          </Link>
+          <div className='relative flex-grow'>
+            <div className='bg-black p-2 pl-4 w-fit h-full'>
+              <div className='grid gap-2 grid-cols-[repeat(2,auto)] grid-rows-[repeat(2,auto)]'>
+                <label className='relative col-start-1 row-start-1 row-span-2 inline-block w-4'>
+                  <input
+                    type='checkbox'
+                    id='sound-effects'
+                    className='sr-only peer'
+                    checked={!showSelected}
+                    onChange={() => {}}
+                  />
+                  <span className='absolute peer-checked:translate-y-3/5 cursor-pointer inset-0 transition-all duration-50 ease-accel rounded-full'>
+                    <img src='/images/arrow.png' className='-rotate-90 w-4' />
+                  </span>
+                </label>
+
+                <p
+                  className={`font-poke text-xs w-fit col-start-2 row-start-1 ${data.showId ? 'text-white' : 'text-neutral-500'}`}
+                >
+                  Start Show
+                </p>
+                <p className='font-poke text-xs w-fit col-start-2 row-start-2'>
+                  See Program
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
       <Controller
-        onUp={() => {}}
-        onDown={() => {}}
+        onUp={() => {
+          if (showSelected || !data.showId) {
+            haptics.pulse({ count: 2, gap: 10 })
+            return
+          }
+          haptics.once()
+          showSelectedAssign(true)
+        }}
+        onDown={() => {
+          if (!showSelected) {
+            haptics.pulse({ count: 2, gap: 10 })
+            return
+          }
+          haptics.once()
+          showSelectedAssign(false)
+        }}
         onLeft={() => {}}
         onRight={() => {}}
-        onA={() => {}}
+        onA={() => {
+          navigation({ to: showSelected ? '/show' : '/program' })
+        }}
         onB={() => {}}
       />
     </main>
