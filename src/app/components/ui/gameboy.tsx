ae1703e2244d9133e3affa9c7ea5d66c7c855b49
Joshua Richardson <jrichardson@wearebond.com>
2025-07-25 23:24:26 -0700

    fix(style): enforce max width on gameboy screen

--- src/app/components/ui/gameboy.tsx
+++ src/app/components/ui/gameboy.tsx
@@ -4,10 +4,14 @@ import { Controller } from './controller'
 
 export function GameBoyFrame(props: PropsWithChildren) {
   return (
-    <main className="grid min-h-screen grid-rows-[1fr_auto]">
-      <ScreenFrame>{props.children}</ScreenFrame>
+    <main className="grid h-screen grid-cols-[1fr_minmax(auto,500px)_1fr] md:bg-blue-200 md:py-12">
+      <div />
+      <div className="grid grid-rows-[1fr_auto] overflow-clip md:rounded-b-lg md:shadow-xl/50">
+        <ScreenFrame>{props.children}</ScreenFrame>
 
-      <Controller />
+        <Controller />
+      </div>
+      <div />
     </main>
   )
 }
