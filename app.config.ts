f06d1e736db90fb28fa3910699935c3c8e3c4cc7
Joshua Richardson <jrichardson@wearebond.com>
2025-07-27 13:21:59 -0700

    prettier

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from '@tailwindcss/vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from '@tanstack/start/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   tsr: {
