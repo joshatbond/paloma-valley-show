f85c745b59a00b034f8e35f962a9192f4c7a8af4
Joshua Richardson <josh@cursedtale.com>
2025-07-29 08:23:51 -0700

    fix(battle): correctly sized stage rendered

--- src/app/components/battle/index.tsx
+++ src/app/components/battle/index.tsx
@@ -1,18 +1,33 @@
-import { Application } from '@pixi/react'
-import { useRef } from 'react'
+import { Stage } from '@pixi/react'
+import { useEffect, useRef, useState } from 'react'
 
 export function BattleSimulator() {
+  const [width, widthAssign] = useState(160)
+  const [height, heightAssign] = useState(144)
   const ref = useRef<HTMLDivElement>(null)
 
+  useEffect(() => {
+    if (ref.current) {
+      const { clientWidth, clientHeight } = ref.current
+      let scale = 4
+      while (clientHeight < 144 * scale || clientWidth < 160 * scale) {
+        scale -= 1
+      }
+      widthAssign(160 * scale)
+      heightAssign(144 * scale)
+    }
+  }, [ref.current])
+
   return (
-    <div ref={ref} className="grid h-full">
-      <Application
-        width={ref.current?.clientWidth}
-        height={ref.current?.clientHeight}
-        sharedTicker
-        autoStart
-        resizeTo={ref}
-      ></Application>
+    <div ref={ref} className="absolute inset-0 grid place-content-center">
+      <Stage
+        width={width}
+        height={height}
+        options={{
+          backgroundColor: 0xf8f8f8,
+          resizeTo: ref.current ?? undefined,
+        }}
+      ></Stage>
     </div>
   )
 }
